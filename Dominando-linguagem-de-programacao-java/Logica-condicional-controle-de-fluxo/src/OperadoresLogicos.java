de6b5e1bafa8e4997aa5d0d8de64b0bac4b2b5ee
jorge tranin <antoniotraninjorge@gmail.com>
2023-03-29 11:06:06 -0400

    Resolução do desafio OperadoresLogicos.java

--- Dominando-linguagem-de-programacao-java/Logica-condicional-controle-de-fluxo/src/OperadoresLogicos.java
+++ Dominando-linguagem-de-programacao-java/Logica-condicional-controle-de-fluxo/src/OperadoresLogicos.java
@@ -1,2 +1,31 @@
 public class OperadoresLogicos {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = true;
+        boolean b4 = false;
+
+// Comparação lógica de igualdade (==)
+        System.out.println("b1 == b2: " + (b1 == b2)); // false
+        System.out.println("b1 == b3: " + (b1 == b3)); // true
+
+// Comparação lógica de diferença (!=)
+        System.out.println("b2 != b3: " + (b2 != b3)); // true
+        System.out.println("b1 != b4: " + (b1 != b4)); // true
+
+// Combinação de comparações lógicas usando operadores booleanos
+        System.out.println("(b1 == b3) && (b2 != b4): " + ((b1 == b3) && (b2 != b4))); // true
+        System.out.println("(b1 == b2) || (b3 != b4): " + ((b1 == b2) || (b3 != b4))); // true
+
+// Criando variáveis auxiliares para simplificar a expressão lógica
+        boolean cond1 = (b1 == b3);
+        boolean cond2 = (b2 != b4);
+
+// Combinação das condições auxiliares usando operadores booleanos
+        boolean resultado = cond1 && cond2;
+
+// Impressão do resultado
+        System.out.println("Resultado: " + resultado);
+
+    }
 }
